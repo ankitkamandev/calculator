36657b7c335eb3ae3bdfc1b44b11b287a85e1c44
Ankit <ankitkaman.dev@gmail.com>
2026-01-20 05:37:34 +0530

    added operators and control buttons

--- script.js
+++ script.js
@@ -1,19 +1,49 @@
-let displayNum = 0;
-const numeralBtns = document.querySelectorAll(".numeral");
+let displayNum = "";
 const input = document.querySelector(".display");
 
-numeralBtns.forEach((btn) => {
-    
-    // 3. Typo fix: It is ".addEventListener", not ".eventListener"
-    btn.addEventListener("click", (e) => {
-        
-        // 4. In arrow functions, use 'e.target' instead of 'this'
-        const value = e.target.innerText;
-        
-        input.value += value;
-        displayNum += value; 
-        
-        // Optional: Log to check it's working
-        console.log("Current number string:", displayNum);
-    });
+const allInputBtns = document.querySelectorAll(".numeral, .operator");
+
+allInputBtns.forEach((btn) => {
+  btn.addEventListener("click", (e) => {
+    const value = e.target.innerText;
+    input.value += value;
+    displayNum += value;
+    console.log("Current number string:", displayNum);
+  });
 });
+
+// Equal Button
+let equalBtn = document.querySelector(".equal");
+
+equalBtn.addEventListener("click", () => {
+  try {
+    let result = eval(displayNum);
+
+    input.value = result;
+
+    displayNum = result.toString();
+  } catch (error) {
+    input.value = "Error";
+    displayNum = "";
+  }
+});
+
+//  Clear Button
+const clearBtn = document.querySelector(".clear");
+
+if (clearBtn) {
+  clearBtn.addEventListener("click", () => {
+    input.value = "";
+    displayNum = "";
+  });
+}
+
+// 4. Handle Backspace Button
+const bscBtn = document.querySelector(".backspace");
+
+if (bscBtn) {
+  bscBtn.addEventListener("click", () => {
+    displayNum = displayNum.slice(0, -1);
+    input.value = displayNum;
+  });
+}
